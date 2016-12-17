eb58b36b8d56df6606587f7d41fc67ee94f4e76e
Helen Chan <hchan22@gmail.com>
2016-12-16 19:37:22 -0500

    notifications

---
+++ app/src/main/java/nyc/c4q/helenchan/nekoclone/AlarmReceiver.java
@@ -0,0 +1,18 @@
+package nyc.c4q.helenchan.nekoclone;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by helenchan on 12/16/16.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+    public static final int REQUEST_CODE = 123;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, NotificationService.class);
+        context.startService(i);
+    }
+}
